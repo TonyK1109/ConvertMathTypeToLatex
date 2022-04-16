74bc6a53526faae32c8a291ee267769f5c8eacc1
Khoi Ngo <ngominhkhoi2014@gmail.com>
2022-04-16 08:30:20 +0700

    add data.json

--- convert.py
+++ convert.py
@@ -1,5 +1,25 @@
 from docxlatex import Document
+import json
+
+suject = "Toán"
+data = {
+    "subject": suject,
+    "body": "Tính thể tích của khối lập phương có cạnh bằng 2",
+    "answers": [
+        {"value": "6"},
+        {"value": "4"},
+        {"value": "2"},
+        {"value": "8", "isCorrect": True}
+    ],
+    "level": 1
+}
+
+datas = []
+datas.append(data)
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(datas, f, ensure_ascii=False,  indent=4)
 
 document = Document('Test1.docx')
 text = document.get_text().split('BẢNG ĐÁP ÁN VÀ HƯỚNG DẪN GIẢI')
-print(text[1])
+for x in text[1].split("Lời giải"):
+    print(x, 'endline \n')
